15250f08466b5080bd893a9e16867e80f940bd13
ahmed haroun <43412584+hamada81993@users.noreply.github.com>
2020-05-09 03:29:57 +0200

    commit

---
+++ momtaz/js/custom_v.js
@@ -0,0 +1,387 @@
+(function($){
+  "use strict";
+  
+  // Preloader 
+	jQuery(window).on('load', function() {
+		jQuery("#status").fadeOut();
+		jQuery("#preloader").delay(350).fadeOut("slow");
+	});
+  
+	// on ready function
+	jQuery(document).ready(function($) {
+	var $this = $(window);
+
+
+	//show hide login form js
+	$('#search_button').on("click", function(e) {
+		$('#search_open').slideToggle();
+		e.stopPropagation(); 
+	});
+
+	$(document).on("click", function(e){
+		if(!(e.target.closest('#search_open'))){	
+			$("#search_open").slideUp();   		
+		}
+   });
+   
+   // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {       
+        $('#return-to-top').fadeIn(200);   
+    } else {
+        $('#return-to-top').fadeOut(200);  
+    }
+});
+$('#return-to-top').on('click', function() {     
+    $('body,html').animate({
+        scrollTop : 0                
+    }, 500);
+});
+   
+   //------------------------ OWL JS Start --------------------//
+   
+   
+   $(document).ready(function() {
+              $('.jp_tittle_slider_content_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+				animateOut: 'bounceInDown',
+				animateIn: 'bounceInDown',
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			
+			$(document).ready(function() {
+              $('.jp_hiring_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  1000: {
+                    items: 4,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			// Featured Products Js
+				$('.ss_featured_products .owl-carousel').owlCarousel({
+					loop:true,
+					margin:0,
+					nav:true,
+					autoplay:false,
+					navText:["PREV" , "NEXT"],
+					dots:true,
+					smartSpeed: 1200,
+					responsive:{
+						0:{
+							items:1
+						},
+						600:{
+							items:1
+						},
+						1000:{
+							items:1
+						}
+					}
+				});
+				
+				
+				$(document).ready(function() {
+              $('.jp_spotlight_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			$(document).ready(function() {
+              $('.jp_best_deal_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			$(document).ready(function() {
+              $('.jp_client_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			//------------------------ OWL JS End--------------------//
+			
+			
+	//-------------------------------------------------------
+    // counter-section
+    //-------------------------------------------------------
+	
+    $('.jp_counter_main_wrapper').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).off('inview');
+        }
+    });
+	
+	
+	 $(document).ready(function() {
+            $('.hs_testi_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                responsiveClass: true,
+                smartSpeed: 1200,
+                navText: ['<i class="flaticon-left-arrow" aria-hidden="true"></i>', '<i class="flaticon-right-arrow" aria-hidden="true"></i>'],
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: true
+                    },
+                    600: {
+                        items: 1,
+                        nav: true
+                    },
+                    1000: {
+                        items: 1,
+                        nav: true,
+                        loop: true,
+                        margin: 20
+                    }
+                }
+            })
+        })
+   
+   
+   $(document).ready(function() {
+              $('.jp_career_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+                responsiveClass: true,
+				smartSpeed: 1200,
+				navText : ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  1000: {
+                    items: 3,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+   
+   
+    //-----js chat box --------//
+	$(function() {
+  var INDEX = 0; 
+  $("#chat-submit").on('click', function(e) {
+    e.preventDefault();
+    var msg = $("#chat-input").val(); 
+    if(msg.trim() == ''){
+      return false;
+    }
+    generate_message(msg, 'self');
+    var buttons = [
+        {
+          name: 'Existing User',
+          value: 'existing'
+        },
+        {
+          name: 'New User',
+          value: 'new'
+        }
+      ];
+    setTimeout(function() {      
+      generate_message(msg, 'user');  
+    }, 1000)
+    
+  })
+  
+  function generate_message(msg, type) {
+    INDEX++;
+    var str="";
+    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
+    str += "          <span class=\"images/content/ct.png\">";
+    str += "            <img src=\"images/content/ct.png\">";
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-"+INDEX).hide().fadeIn(300);
+    if(type == 'self'){
+     $("#chat-input").val(''); 
+    }    
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
+  }  
+  
+  function generate_button_message(msg, buttons){    
+    /* Buttons should be object array 
+      [
+        {
+          name: 'Existing User',
+          value: 'existing'
+        },
+        {
+          name: 'New User',
+          value: 'new'
+        }
+      ]
+    */
+    INDEX++;
+    var btn_obj = buttons.map(function(button) {
+       return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
+    }).join('');
+    var str="";
+    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
+    str += "          <span class=\"images/content/ct2.png\">";
+    str += "            <img src=\"images/content/ct2.png\">";
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "          <div class=\"cm-msg-button\">";
+    str += "            <ul>";   
+    str += btn_obj;
+    str += "            <\/ul>";
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-"+INDEX).hide().fadeIn(300);   
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
+    $("#chat-input").attr("disabled", true);
+  }
+  
+  $(document).delegate(".chat-btn", "click", function() {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
+  
+  $("#chat-circle").on('click', function() {    
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+  $(".chat-box-toggle").on('click', function() {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+})
+   
+   
+	
+	});
+})();   
